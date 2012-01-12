05e6c6979c8476c23f1ffdfa5af5c28a45616eaf
Dmitriy Paramonov <dev@divniy.com>
2012-01-12 04:38:00 +0400

    Story prepared

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,6 +1,18 @@
 class Story < ActiveRecord::Base
-  attr_accessible :title, :content
+  attr_accessor :state_name
+  attr_accessible :title, :content, :user_id, :state, :state_name
 
   belongs_to :user
   default_scope :order => "stories.created_at DESC"
+
+  STATE_TABLE = { new: 1, accepted: 2, stared: 3, finished: 4, rejected: nil }
+
+  def state_name
+    @state_name = STATE_TABLE.key(state)
+  end
+
+  def state_name=(value)
+    @state, @state_name = STATE_TABLE[value], value
+  end
+
 end
--- test/unit/story_test.rb
+++ test/unit/story_test.rb
@@ -6,7 +6,7 @@ class StoryTest < ActiveSupport::TestCase
     @attr = { title: "Test Task", content: "Some text" }
   end
 
-  test "should create new task" do
+  test "should create new story" do
     story = @user.stories.create(@attr)
     assert story.save
   end
@@ -25,12 +25,13 @@ class StoryTest < ActiveSupport::TestCase
   test "should have defaults scope by created_at DESC" do
     story1 = stories(:one)
     story2 = stories(:two)
-    assert_equal(Story.all,[story2, story1])
+    assert_equal Story.all, [story2, story1]
   end
 
   #State name tests
   test "should have right state_name associated with state" do
-
+    story = stories(:one).find
+    assert_equal Story::STATE_TABLE(story.state_name), story.state
   end
   test "should have 'new' state_name "
 
